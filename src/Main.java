24bed802ff1a32265954e22c11ff6e2e0f5bb9ee
Olesya Sumatokhina <olesya_po@mail.ru>
2023-11-28 13:42:03 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int replenish = 800;
-        int check = balance + replenish;
-        int bonus = replenish / 100;
-        if (replenish >= 1000) {
-            System.out.println(check + bonus);
+        int replenish = 1200;
+        int check;
+        int bonus;
+        if (replenish > 1000) {
+            check = balance + replenish;
+            bonus = replenish / 100;
+            System.out.println(check);
+            {
+                System.out.println(bonus);
+            }
         } else {
+            check = balance + replenish;
             System.out.println(check);
         }
     }
