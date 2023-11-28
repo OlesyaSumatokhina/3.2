096a328ec43f3356fe5d0fb864017c4ac7761c59
Olesya Sumatokhina <olesya_po@mail.ru>
2023-11-28 11:19:49 +0300

    delete100

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int replenish = 1800;
+        int replenish = 800;
         int check = balance + replenish;
-        int difference = check - 100;
-        int bonus = difference / 100;
+        int bonus = replenish / 100;
         if (replenish >= 1000) {
             System.out.println(check + bonus);
         } else {
